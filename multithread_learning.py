ac69c30c0eea3932d25fdd7aadd0476a5191ff11
wangzi <785692920@qq.com>
2017-06-07 17:15:28 +0800

    看来单CPU最大线程是8

--- multithread_learning.py
+++ multithread_learning.py
@@ -29,9 +29,22 @@ def print_time(threadName, delay, counter):
 # create new thread
 thread1 = myThread(1, "Thread-1", 1)
 thread2 = myThread(2, "Thread-2", 2)
+thread3 = myThread(3, "Thread-3", 2)
+thread4 = myThread(4, "Thread-4", 4)
+thread5 = myThread(5, "Thread-5", 3)
+thread6 = myThread(6, "Thread-6", 1)
+thread7 = myThread(7, "Thread-7", 1)
+thread8 = myThread(8, "Thread-8", 1)
+thread9 = myThread(9, "Thread-9", 1)
 
 # thread start
 thread1.start()
 thread2.start()
+thread3.start()
+thread4.start()
+thread5.start()
+thread6.start()
+thread7.start()
+thread8.start()
 
 print "Exiting Main Thread"
